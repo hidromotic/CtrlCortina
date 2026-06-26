de475be63e033b50918d52eb3213972c32a9da1c
hidromotic <hidromotic@gmail.com>
2026-06-25 22:06:38 -0300

    Update CtrlCortinaREPO.ino

--- CtrlCortinaREPO/CtrlCortinaREPO.ino
+++ CtrlCortinaREPO/CtrlCortinaREPO.ino
@@ -11,6 +11,21 @@
 // Manuel Ponce
 // Elizondo Julian
 
+/*
+Asignaciones para completar el código:
+
+Definiciones --> BASCAL, Francisco Emanuel
+Setup --> PONCE, Manuel Andres
+loop() --> BUSTOS, Exequiel Yoel
+TestFC() --> BUSS, Agustin
+TestPULSADORES() --> BASCAL, Francisco Emanuel
+LedTest () --> GERBUEZ SOLANGE, Abigail
+CtrlMotor(void) --> FANJUL, Luca Miguel
+CtrlCortina() --> SCHIEL, Elias Martin
+CtrlPulsador(void) --> BUSS, Agustin
+RecepcionSerie(void) --> BASCAL, Francisco Emanuel
+TransmisionSerie(void) --> BUSTOS, Exequiel Yoel
+*/
 void setup() {
   // put your setup code here, to run once:
 
